a4004576fcfb1ca23cbbdd8f4f5cf66e2c455277
Ravi C <chilakaraviteja@yahoo.com>
2024-08-27 22:24:57 +0100

    styling

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -8,7 +8,7 @@ const ItemList = ({ items }) => {
         <div key={item?.card?.info?.id} className=" p-4 border-b-2">
           <div className="flex justify-between">
             <div className="w-9/12 mr-4">
-              <p className="text-lg font-bold">{item?.card?.info?.name}</p>
+              <p className="text-lg font-bold ">{item?.card?.info?.name}</p>
               <p className="text-sm font-bold">
                 {item?.card?.info?.ratings?.aggregatedRating?.rating}⭐
               </p>
